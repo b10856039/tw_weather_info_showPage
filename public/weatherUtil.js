b176e41556f5f2d9bb376c783dcc9569a90b0235
b10856039 <ycenz234@gmail.com>
2024-04-08 20:02:58 +0800

    fix cors problem version

--- public/weatherUtil.js
+++ public/weatherUtil.js
@@ -32,7 +32,7 @@ export class Location{
 export class WeatherAPI{
 
     //後端伺服器API
-    static _apiBaseUrl =import.meta.env.VUE_APP_API_URL //'http://127.0.0.1:3000/api/v1'
+    static _apiBaseUrl =import.meta.env.VITE_API_URL //'http://127.0.0.1:3000/api/v1'
 
     static getAPIBaseUrl(){
         return WeatherAPI._apiBaseUrl
