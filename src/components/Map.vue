e7a3288c1adc10b6410568a071d405eca58564a4
b10856039 <ycenz234@gmail.com>
2024-04-17 12:56:20 +0800

    hanlde locate data

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -262,11 +262,12 @@ export default {
         try {
             const response = await fetch(reverseGeocodingAPI);
             const data = await response.json();
+            console.log(data)
             this.cityName = data.address.city; 
             await this.toggleMap(false);
             this.showCity = false;                    
             await this.generatorTownSelecter();
-            this.townName = data.address.suburb;
+            this.townName = data.address.suburb;          
             await this.map.search(this.showCity,this.cityName,this.townName)
         } catch (error) {
             console.error('Error fetching reverse geocoding data:', error);
